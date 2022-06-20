42a3b208a2807526bd081ef8efe5ec179e6db49f
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-06-20 19:58:00 +0900

    Add files via upload

---
+++ index01.py
@@ -0,0 +1,10 @@
+a={'name':'pey','phone':'01011112222','birth':'1111'}
+print(a.keys())
+print(a.values())
+print(a.items())
+for k in a.keys():
+    print(k)
+for v in a.values():
+    print(v)
+for i in a.items():
+    print(i)
