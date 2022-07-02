9957ac4c1adb3d521798d06c658082c7f827d63f
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-07-02 19:33:13 +0900

    Add files via upload

---
+++ practice01.py
@@ -0,0 +1,15 @@
+animal = "강아지"
+name = "댕댕이"
+age = 5
+hobby = "눕기"
+is_adult = age >= 3
+
+'''이렇게 하면
+여러문장이
+주석처리가 된다.'''
+
+print("우리집"+animal+"의 이름은 "+name+"입니다.")
+hobby = "공놀이"
+print(name,"는",age,"살이며,",hobby,"을 좋아합니다.")
+
+# 파이참, vscode, vs등 대부분 idle에서의 주석 단축키 ctrl + /
