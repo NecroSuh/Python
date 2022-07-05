0e69e83a250902c3b212a7a2a16421b35153db1e
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-07-05 20:33:44 +0900

    Add files via upload

---
+++ practice15.py
@@ -0,0 +1,12 @@
+students = [1,2,3,4,5]
+print(students)
+students = [i+100 for i in students]
+print(students)
+
+students = ["Iron man", "Thor", "Dr.Strange"]
+studets =[len(i) for i in students]
+print(students)
+
+students = ["Iron man", "Thor", "Dr.Strange"]
+students = [i.upper() for i in students]
+print(students)
