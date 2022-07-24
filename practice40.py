756c6843bb26622341678dd11c80c3c236d8addb
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-07-24 21:43:58 +0900

    Add files via upload

---
+++ practice40.py
@@ -0,0 +1,22 @@
+#import practice39
+#practice39.price(3)
+#practice39.price_morning(4)
+#practice39.price_soldier(5)
+
+#import practice39 as pr
+#pr.price(3)
+#pr.price_morning(4)
+#pr.price_soldier(5)
+
+#from practice39 import *
+#price(3)
+#price_morning(4)
+#price_soldier(5)
+
+#from practice39 import price, price_morning
+#price(5)
+#price_morning(6)
+
+from practice39 import price_soldier as pr
+pr(5)
+
