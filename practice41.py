d63f2c1c830befab5f298d33eee67cf531db4ec3
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-07-28 22:25:33 +0900

    Add files via upload

---
+++ practice41.py
@@ -0,0 +1,11 @@
+#import thailand
+#trip_to = thailand.ThailandPackage()
+#trip_to.detail()
+
+from thailand import ThailandPackage
+trip_to = ThailandPackage()
+trip_to.detail()
+
+import vietnam
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
